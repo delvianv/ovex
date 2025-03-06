bbbe3c9e51ea08e5bf90e9e0f591ef3b48ef145c
Delvian Valentine <delvian.valentine@gmail.com>
2025-03-06 12:27:57 +0200

    Update amount output when amount input changes

--- components/CurrencyConversion.tsx
+++ components/CurrencyConversion.tsx
@@ -1,13 +1,36 @@
+import { useContext, useEffect, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 import { Color } from "../constants/Color";
 import { FontFamily } from "../constants/FontFamily";
 import { FontSize } from "../constants/FontSize";
+import { AmountContext } from "../contexts/AmountContext";
+
+import {
+  Currency,
+  CurrencyContext,
+  SourceCurrency,
+} from "../contexts/CurrencyContext";
 
 export default function CurrencyConversion() {
+  const sourceCurrency = useContext(SourceCurrency);
+  const amount = useContext(AmountContext);
+  const currencies = useContext(CurrencyContext);
+
+  const [currency, setCurrency] = useState<Currency>();
+
+  useEffect(() => {
+    setCurrency(currencies.find((currency) => currency.id === sourceCurrency));
+  }, [sourceCurrency]);
+
   return (
     <View style={styles.container}>
-      <Text style={styles.sourceOutput}>$0 US Dollars</Text>
+      <Text style={styles.sourceOutput}>
+        {currency
+          ? `${currency.symbol}${amount} ${currency.name}`
+          : "$0 US Dollars"}{" "}
+        =
+      </Text>
       <Text style={styles.destinationOutput}>0 Bitcoin</Text>
     </View>
   );
