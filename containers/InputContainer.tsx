a6eeccb83bf634faa5dfb4c9503e181800f77cc6
Delvian Valentine <delvian.valentine@gmail.com>
2025-06-05 17:02:55 +0200

    Change background and border colours of InputContainer

--- containers/InputContainer.tsx
+++ containers/InputContainer.tsx
@@ -1,8 +1,6 @@
 import { PropsWithChildren } from "react";
 import { StyleSheet, View } from "react-native";
 
-import { colors } from "@/constants/Colors";
-
 export default function InputContainer({ children }: PropsWithChildren) {
   return <View style={styles.container}>{children}</View>;
 }
@@ -11,10 +9,12 @@ const styles = StyleSheet.create({
   container: {
     flexDirection: "row",
     alignItems: "center",
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: "#A6BBE229",
+    backgroundColor: "#FFFFFF",
+    elevation: 1,
     height: 56,
     paddingHorizontal: 16,
-    elevation: 1,
-    borderRadius: 4,
-    backgroundColor: colors.background,
   },
 });
